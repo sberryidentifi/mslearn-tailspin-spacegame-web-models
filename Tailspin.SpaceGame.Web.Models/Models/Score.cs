1e451121d1696d9c4aa7fbd1adbcfecff5713af9
sberryidentifi <sberry@identifi.net>
2020-01-10 10:43:04 -0500

    Add GameStyle property

--- Tailspin.SpaceGame.Web.Models/Models/Score.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Score.cs
@@ -23,5 +23,6 @@ namespace TailSpin.SpaceGame.Web.Models
         // The game style (difficulty) the score is associated with.
         [JsonProperty(PropertyName = "gameStyle")]
         public string GameStyle { get; set; }
+        
     }
 }
